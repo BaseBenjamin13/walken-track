f017a0927e9629e40c26a60e86d99967e649ce97
BaseBenjamin13 <benmorgiewicz@gmail.com>
2024-07-09 17:54:05 -0400

    implement chart empty states

--- Walken Track/Charts/WeightDiffBarChart.swift
+++ Walken Track/Charts/WeightDiffBarChart.swift
@@ -43,48 +43,53 @@ struct WeightDiffBarChart: View {
             .foregroundStyle(.secondary)
             .padding(.bottom, 12)
             
-            Chart {
-                if let selectedData {
-                    RuleMark(x: .value("Selected Data", selectedData.date, unit: .day))
-                        .foregroundStyle(Color.secondary.opacity(0.4))
-                        .offset(y: -10)
-                        .annotation(
-                            position: .top,
-                            spacing: 0,
-                            overflowResolution: .init(
-                                x: .fit(to: .chart),
-                                y: .disabled)
-                        ) {
-                            annotationView
-                        }
+            if chartData.isEmpty {
+                ChartEmptyView(systemImageName: "chart.bar", title: "No Data", description: "There is no weight data from the Health App.")
+            } else {
+                Chart {
+                    if let selectedData {
+                        RuleMark(x: .value("Selected Data", selectedData.date, unit: .day))
+                            .foregroundStyle(Color.secondary.opacity(0.4))
+                            .offset(y: -10)
+                            .annotation(
+                                position: .top,
+                                spacing: 0,
+                                overflowResolution: .init(
+                                    x: .fit(to: .chart),
+                                    y: .disabled)
+                            ) {
+                                annotationView
+                            }
+                    }
+
+                    ForEach(chartData) { weightDiff in
+                        BarMark(
+                            x: .value("Date", weightDiff.date, unit: .day),
+                            y: .value("Weight Diff", weightDiff.value)
+                        )
+                        .foregroundStyle(weightDiff.value >= 0 ? Color.red.gradient : Color.indigo.gradient)
+                        .opacity(rawSelectedDate == nil ||
+                            weightDiff.date == selectedData?.date ? 1.0 : 0.4
+                        )
+                        .cornerRadius(8)
+                    }
+                }
+                .frame(height: 150)
+                .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
+                .chartXAxis {
+                    AxisMarks(values: .stride(by: .day)) {
+                        AxisValueLabel(format: .dateTime.weekday(), centered: true)
+                    }
+                }
+                .chartYAxis {
+                    AxisMarks { value in
+                        AxisGridLine()
+                            .foregroundStyle(Color.secondary.opacity(0.3))
+                        
+                        AxisValueLabel()
+                    }
                 }
 
-                ForEach(chartData) { weightDiff in
-                    BarMark(
-                        x: .value("Date", weightDiff.date, unit: .day),
-                        y: .value("Weight Diff", weightDiff.value)
-                    )
-                    .foregroundStyle(weightDiff.value >= 0 ? Color.red.gradient : Color.indigo.gradient)
-                    .opacity(rawSelectedDate == nil ||
-                        weightDiff.date == selectedData?.date ? 1.0 : 0.4
-                    )
-                    .cornerRadius(8)
-                }
-            }
-            .frame(height: 150)
-            .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
-            .chartXAxis {
-                AxisMarks(values: .stride(by: .day)) {
-                    AxisValueLabel(format: .dateTime.weekday(), centered: true)
-                }
-            }
-            .chartYAxis {
-                AxisMarks { value in
-                    AxisGridLine()
-                        .foregroundStyle(Color.secondary.opacity(0.3))
-                    
-                    AxisValueLabel()
-                }
             }
         }
         .padding()
